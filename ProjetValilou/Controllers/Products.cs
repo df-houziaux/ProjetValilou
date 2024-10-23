0062ed9cee5841b5e4d5166e32412d552e4ece1d
David Houziaux <df.houziaux@gmail.com>
2024-10-23 12:50:51 +0200

    ajout  des photo vanille et rosee

--- ProjetValilou/Controllers/Products.cs
+++ ProjetValilou/Controllers/Products.cs
@@ -26,32 +26,32 @@ namespace ProjetValilou.Controllers
 
 
             // Bougies Vanille
-            new Product { Id = 13, Name = "Bougie Vanille ", ImageUrl = "/images/vanille1.jpg", Description = "Bougie parfumée à la vanille ", Price = 15.99m, Ingredients = "Vanille, Cire de soja" },
-            new Product { Id = 14, Name = "Bougie Vanille ", ImageUrl = "/images/vanille2.jpg", Description = "Bougie parfumée à la vanille ", Price = 16.99m, Ingredients = "Vanille, Cire de soja" },
-            new Product { Id = 15, Name = "Bougie Vanille ", ImageUrl = "/images/vanille3.jpg", Description = "Bougie parfumée à la vanille ", Price = 17.99m, Ingredients = "Vanille, Cire de soja" },
-            new Product { Id = 16, Name = "Bougie Vanille ", ImageUrl = "/images/vanille4.jpg", Description = "Bougie parfumée à la vanille ", Price = 18.99m, Ingredients = "Vanille, Cire de soja" },
-            new Product { Id = 17, Name = "Bougie Vanille ", ImageUrl = "/images/vanille5.jpg", Description = "Bougie parfumée à la vanille ", Price = 19.99m, Ingredients = "Vanille, Cire de soja" },
-            new Product { Id = 18, Name = "Bougie Vanille ", ImageUrl = "/images/vanille6.jpg", Description = "Bougie parfumée à la vanille ", Price = 20.99m, Ingredients = "Vanille, Cire de soja" },
-            new Product { Id = 19, Name = "Bougie Vanille ", ImageUrl = "/images/vanille7.jpg", Description = "Bougie parfumée à la vanille ", Price = 21.99m, Ingredients = "Vanille, Cire de soja" },
-            new Product { Id = 20, Name = "Bougie Vanille ", ImageUrl = "/images/vanille8.jpg", Description = "Bougie parfumée à la vanille ", Price = 22.99m, Ingredients = "Vanille, Cire de soja" },
-            new Product { Id = 21, Name = "Bougie Vanille ", ImageUrl = "/images/vanille9.jpg", Description = "Bougie parfumée à la vanille ", Price = 23.99m, Ingredients = "Vanille, Cire de soja" },
-            new Product { Id = 22, Name = "Bougie Vanille ", ImageUrl = "/images/vanille10.jpg", Description = "Bougie parfumée à la vanille ", Price = 24.99m, Ingredients = "Vanille, Cire de soja" },
-            new Product { Id = 23, Name = "Bougie Vanille ", ImageUrl = "/images/vanille11.jpg", Description = "Bougie parfumée à la vanille ", Price = 25.99m, Ingredients = "Vanille, Cire de soja" },
-            new Product { Id = 24, Name = "Bougie Vanille ", ImageUrl = "/images/vanille12.jpg", Description = "Bougie parfumée à la vanille ", Price = 26.99m, Ingredients = "Vanille, Cire de soja" },
+            new Product { Id = 13, Name = "Bougie Vanille ", ImageUrl = "/images/bougievanille1.jpg", Description = "Bougie parfumée à la vanille ", Price = 15.99m, Ingredients = "Vanille, Cire de soja" },
+            new Product { Id = 14, Name = "Bougie Vanille ", ImageUrl = "/images/bougievanille2.jpg", Description = "Bougie parfumée à la vanille ", Price = 16.99m, Ingredients = "Vanille, Cire de soja" },
+            new Product { Id = 15, Name = "Bougie Vanille ", ImageUrl = "/images/bougievanille3.jpg", Description = "Bougie parfumée à la vanille ", Price = 17.99m, Ingredients = "Vanille, Cire de soja" },
+            new Product { Id = 16, Name = "Bougie Vanille ", ImageUrl = "/images/bougievanille4.jpg", Description = "Bougie parfumée à la vanille ", Price = 18.99m, Ingredients = "Vanille, Cire de soja" },
+            new Product { Id = 17, Name = "Bougie Vanille ", ImageUrl = "/images/bougievanille5.jpg", Description = "Bougie parfumée à la vanille ", Price = 19.99m, Ingredients = "Vanille, Cire de soja" },
+            new Product { Id = 18, Name = "Bougie Vanille ", ImageUrl = "/images/bougievanille6.jpg", Description = "Bougie parfumée à la vanille ", Price = 20.99m, Ingredients = "Vanille, Cire de soja" },
+            new Product { Id = 19, Name = "Bougie Vanille ", ImageUrl = "/images/bougievanille7.jpg", Description = "Bougie parfumée à la vanille ", Price = 21.99m, Ingredients = "Vanille, Cire de soja" },
+            new Product { Id = 20, Name = "Bougie Vanille ", ImageUrl = "/images/bougievanille8.jpg", Description = "Bougie parfumée à la vanille ", Price = 22.99m, Ingredients = "Vanille, Cire de soja" },
+            new Product { Id = 21, Name = "Bougie Vanille ", ImageUrl = "/images/bougievanille9.jpg", Description = "Bougie parfumée à la vanille ", Price = 23.99m, Ingredients = "Vanille, Cire de soja" },
+            new Product { Id = 22, Name = "Bougie Vanille ", ImageUrl = "/images/bougievanille10.jpg", Description = "Bougie parfumée à la vanille ", Price = 24.99m, Ingredients = "Vanille, Cire de soja" },
+            new Product { Id = 23, Name = "Bougie Vanille ", ImageUrl = "/images/bougievanille11.jpg", Description = "Bougie parfumée à la vanille ", Price = 25.99m, Ingredients = "Vanille, Cire de soja" },
+            new Product { Id = 24, Name = "Bougie Vanille ", ImageUrl = "/images/bougievanille12.jpg", Description = "Bougie parfumée à la vanille ", Price = 26.99m, Ingredients = "Vanille, Cire de soja" },
 
             // Bougies Rosée
-            new Product { Id = 25, Name = "Bougie Rosée ", ImageUrl = "/images/rosee1.jpg", Description = "Bougie parfumée à la rosée ", Price = 17.99m, Ingredients = "Rosée, Cire de soja" },
-            new Product { Id = 26, Name = "Bougie Rosée ", ImageUrl = "/images/rosee2.jpg", Description = "Bougie parfumée à la rosée ", Price = 18.99m, Ingredients = "Rosée, Cire de soja" },
-            new Product { Id = 27, Name = "Bougie Rosée ", ImageUrl = "/images/rosee3.jpg", Description = "Bougie parfumée à la rosée ", Price = 19.99m, Ingredients = "Rosée, Cire de soja" },
-            new Product { Id = 28, Name = "Bougie Rosée ", ImageUrl = "/images/rosee4.jpg", Description = "Bougie parfumée à la rosée ", Price = 20.99m, Ingredients = "Rosée, Cire de soja" },
-            new Product { Id = 29, Name = "Bougie Rosée ", ImageUrl = "/images/rosee5.jpg", Description = "Bougie parfumée à la rosée ", Price = 21.99m, Ingredients = "Rosée, Cire de soja" },
-            new Product { Id = 30, Name = "Bougie Rosée ", ImageUrl = "/images/rosee6.jpg", Description = "Bougie parfumée à la rosée ", Price = 22.99m, Ingredients = "Rosée, Cire de soja" },
-            new Product { Id = 31, Name = "Bougie Rosée ", ImageUrl = "/images/rosee7.jpg", Description = "Bougie parfumée à la rosée ", Price = 23.99m, Ingredients = "Rosée, Cire de soja" },
-            new Product { Id = 32, Name = "Bougie Rosée ", ImageUrl = "/images/rosee8.jpg", Description = "Bougie parfumée à la rosée ", Price = 24.99m, Ingredients = "Rosée, Cire de soja" },
-            new Product { Id = 33, Name = "Bougie Rosée ", ImageUrl = "/images/rosee9.jpg", Description = "Bougie parfumée à la rosée ", Price = 25.99m, Ingredients = "Rosée, Cire de soja" },
-            new Product { Id = 34, Name = "Bougie Rosée ", ImageUrl = "/images/rosee10.jpg", Description = "Bougie parfumée à la rosée ", Price = 26.99m, Ingredients = "Rosée, Cire de soja" },
-            new Product { Id = 35, Name = "Bougie Rosée ", ImageUrl = "/images/rosee11.jpg", Description = "Bougie parfumée à la rosée ", Price = 27.99m, Ingredients = "Rosée, Cire de soja" },
-            new Product { Id = 36, Name = "Bougie Rosée ", ImageUrl = "/images/rosee12.jpg", Description = "Bougie parfumée à la rosée ", Price = 28.99m, Ingredients = "Rosée, Cire de soja" }
+            new Product { Id = 25, Name = "Bougie Rosée ", ImageUrl = "/images/bougierosee1.jpg", Description = "Bougie parfumée à la rosée ", Price = 17.99m, Ingredients = "Rosée, Cire de soja" },
+            new Product { Id = 26, Name = "Bougie Rosée ", ImageUrl = "/images/bougierosee2.jpg", Description = "Bougie parfumée à la rosée ", Price = 18.99m, Ingredients = "Rosée, Cire de soja" },
+            new Product { Id = 27, Name = "Bougie Rosée ", ImageUrl = "/images/bougierosee3.jpg", Description = "Bougie parfumée à la rosée ", Price = 19.99m, Ingredients = "Rosée, Cire de soja" },
+            new Product { Id = 28, Name = "Bougie Rosée ", ImageUrl = "/images/bougierosee4.jpg", Description = "Bougie parfumée à la rosée ", Price = 20.99m, Ingredients = "Rosée, Cire de soja" },
+            new Product { Id = 29, Name = "Bougie Rosée ", ImageUrl = "/images/bougierosee5.jpg", Description = "Bougie parfumée à la rosée ", Price = 21.99m, Ingredients = "Rosée, Cire de soja" },
+            new Product { Id = 30, Name = "Bougie Rosée ", ImageUrl = "/images/bougierosee6.jpg", Description = "Bougie parfumée à la rosée ", Price = 22.99m, Ingredients = "Rosée, Cire de soja" },
+            new Product { Id = 31, Name = "Bougie Rosée ", ImageUrl = "/images/bougierosee7.jpg", Description = "Bougie parfumée à la rosée ", Price = 23.99m, Ingredients = "Rosée, Cire de soja" },
+            new Product { Id = 32, Name = "Bougie Rosée ", ImageUrl = "/images/bougierosee8.jpg", Description = "Bougie parfumée à la rosée ", Price = 24.99m, Ingredients = "Rosée, Cire de soja" },
+            new Product { Id = 33, Name = "Bougie Rosée ", ImageUrl = "/images/bougierosee9.jpg", Description = "Bougie parfumée à la rosée ", Price = 25.99m, Ingredients = "Rosée, Cire de soja" },
+            new Product { Id = 34, Name = "Bougie Rosée ", ImageUrl = "/images/bougierosee10.jpg", Description = "Bougie parfumée à la rosée ", Price = 26.99m, Ingredients = "Rosée, Cire de soja" },
+            new Product { Id = 35, Name = "Bougie Rosée ", ImageUrl = "/images/bougierosee11.jpg", Description = "Bougie parfumée à la rosée ", Price = 27.99m, Ingredients = "Rosée, Cire de soja" },
+            new Product { Id = 36, Name = "Bougie Rosée ", ImageUrl = "/images/bougierosee12.jpg", Description = "Bougie parfumée à la rosée ", Price = 28.99m, Ingredients = "Rosée, Cire de soja" }
         };
 
         // Afficher tous les produits
